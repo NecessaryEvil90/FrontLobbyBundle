c293a8efa3a2fc87024bafaf61db1a96a91be1aa
Mishura Yuriy <Yuriy.Mishura@modera.net>
2016-03-07 18:19:49 +0200

    controller updated

--- Controller/LobbyController.php
+++ Controller/LobbyController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class LobbyController extends Controller
 {
@@ -15,6 +16,32 @@ class LobbyController extends Controller
      */
     public function lobbyAction(Request $request)
     {
-        return new Response('yes');
+        return $this->render('base.html.twig');
+    }
+
+    /**
+     * @Route("/getLobbyList", name="front_lobby_list")
+     */
+    public function getLobbyListAction(Request $request)
+    {
+        return new JsonResponse(array(
+            array(
+                'name' => 'test lobby',
+                'created' => new \DateTime(),
+                'players' => 4,
+                'size' => 'big',
+            ),
+            array(
+                'name' => 'test lobby2',
+                'created' => new \DateTime(),
+                'players' => 2,
+                'size' => 'small',
+            ),
+        ));
+    }
+
+    public function getLobbyService()
+    {
+        $this->container->get('');
     }
 }
